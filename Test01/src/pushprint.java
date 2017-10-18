f2762a65d34802dc7fabddb3740d8745d8dfb64d
jhsong <jhsong@naver.com>
2017-10-18 15:37:08 +0900

    remote

--- Test01/src/pushprint.java
+++ Test01/src/pushprint.java
@@ -4,6 +4,7 @@ public class pushprint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello PushPrint");
+		System.out.println("Remote fetch")
 	}
 
 }
